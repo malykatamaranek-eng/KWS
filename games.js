19a282c5bb0f8908139bf90b48d9d99d20b987de
malykatamaranek-eng <malykatamaranek@gmail.com>
2026-02-26 18:19:54 +0100

    Create games.js file with gambling games logic

---
+++ games.js
@@ -0,0 +1,35 @@
+// games.js
+
+// Roulette
+function roulette() {
+    // Logic for roulette game
+}
+
+// Slots
+function slots() {
+    // Logic for slots game
+}
+
+// Blackjack
+function blackjack() {
+    // Logic for blackjack game
+}
+
+// Dice
+function dice() {
+    // Logic for dice game
+}
+
+// Coinflip
+function coinflip() {
+    // Logic for coinflip game
+}
+
+// Exporting all functions if needed
+module.exports = {
+    roulette,
+    slots,
+    blackjack,
+    dice,
+    coinflip,
+};
